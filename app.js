bcda993edcd91af2b61e0caf41d14949a52b2cad
Girish Nair <girishmuraly@gmail.com>
2014-03-07 17:29:30 +0000

    Removed stylus from package.json and removed node_modules

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
